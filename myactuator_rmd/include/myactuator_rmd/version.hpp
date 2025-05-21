4fd4c04c9473826e9e94e3ebce22a9c881ee3e31
taehun-kmu <taehun@kookmin.ac.kr>
2025-05-21 10:54:22 +0900

    feat: Update protocol version to V1.61

--- myactuator_rmd/include/myactuator_rmd/version.hpp
+++ myactuator_rmd/include/myactuator_rmd/version.hpp
@@ -4,14 +4,14 @@
  *    Contains macros for the driver protocol version
  * \author
  *    Tobit Flatscher (github.com/2b-t)
+ *    Taehun Jung (github.com/KMU-KUDOS)
 */
 
 #ifndef MYACTUATOR_RMD__VERSION
 #define MYACTUATOR_RMD__VERSION
-#pragma once
 
-#define MYACTUATOR_RMD_PROTOCOL_MAJOR_VERSION 3
-#define MYACTUATOR_RMD_PROTOCOL_MINOR_VERSION 8
+#define MYACTUATOR_RMD_PROTOCOL_MAJOR_VERSION 1
+#define MYACTUATOR_RMD_PROTOCOL_MINOR_VERSION 61
 
 
 #endif // MYACTUATOR_RMD__VERSION
